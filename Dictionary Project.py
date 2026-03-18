b0e1f3a13fa20c8550de1ec98ef4c7c010e2f0b4
callac2 <callac2@nycstudents.net>
2026-03-18 09:26:06 -0400

    Update Dictionary Project.py

--- Dictionary Project.py
+++ Dictionary Project.py
@@ -5,7 +5,6 @@ item = [
     "department": "women's shirts",
     "description": "100 percent cotton, color: white."
 },
-
 {
     "name": "Blue T-shirt",
     "price": 12.99,
@@ -20,15 +19,15 @@ item = [
 },
 
 {
-    "name": "Red T-shirt",
+    "name": "Black T-shirt",
     "price": 12.99,
     "department": "women's shirts",
-    "description": "100 percent cotton, color: blue"
+    "description": "100 percent cotton, color: black"
 },
 
     ]
 
-for index, item in enumerate(item):
+""" for index, item in enumerate(item):
         print(index, ":", item["name"])
-
-item[0]["Red T-Shirt"]
+ """
+print(item[0]["name"])
